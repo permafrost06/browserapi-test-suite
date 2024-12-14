421e88350f0fe4778a0935a5c35fc27660256b83
Zahin Azmayeen <zahin06@gmail.com>
2024-12-14 21:14:57 +0600

    move top level async to async iife

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import tests from "./tests";
 
-for (const test of tests) {
-    await test.run();
-}
-document.body.innerText = "All tests have finished running"
+(async () => {
+    for (const test of tests) {
+        await test.run();
+    }
+    document.body.innerText = "All tests have finished running"
+})()
