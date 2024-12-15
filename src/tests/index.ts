4edd78f1e4822d932dcd1a0f7b18d9a202d3e7b1
Zahin Azmayeen <zahin06@gmail.com>
2024-12-15 22:54:04 +0600

    add back all tests

--- src/tests/index.ts
+++ src/tests/index.ts
@@ -2,9 +2,9 @@ import { localVideoTest, remoteVideoTest } from "./video";
 import { localAudioTest, remoteAudioTest, localWebAudioTest } from "./audio";
 
 export default [
-    // localVideoTest,
-    // remoteVideoTest,
-    // localAudioTest,
-    // remoteAudioTest,
+    localVideoTest,
+    remoteVideoTest,
+    localAudioTest,
+    remoteAudioTest,
     localWebAudioTest
 ];
