8ca73e533de51af570cd1ee98db195310a9a946d
Zahin Azmayeen <zahin06@gmail.com>
2024-12-18 00:18:31 +0600

    update build config for test page

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
     plugins: [],
     build: {
         sourcemap: true,
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                test: resolve(__dirname, "test.html"),
+            },
+        },
     },
 });
 
