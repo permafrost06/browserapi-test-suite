7b4eb76df3a940b253d7dc3eb2ce1ddd11505cfc
Zahin Azmayeen <zahin06@gmail.com>
2024-12-18 16:38:22 +0600

    add iframe toggler, add delay to avoid simulator page hydration error

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,9 +1,26 @@
-(function() {
+setTimeout(function() {
     'use strict';
+
+    const div = document.createElement("div");
+    div.classList.add("injected-iframe-container");
 
     const frame = document.createElement("iframe");
     frame.src = "http://localhost:5173/";
 
+    const toggle = document.createElement("div");
+    toggle.classList.add("injected-iframe-toggle");
+    toggle.addEventListener("click", () => {
+        const hidden = div.classList.contains("iframe-hidden");
+        if (hidden) {
+            div.classList.remove("iframe-hidden");
+        } else {
+            div.classList.add("iframe-hidden");
+        }
+    });
+
+    div.appendChild(frame);
+    div.appendChild(toggle);
+
     const main = document.querySelector("main");
-    main.insertBefore(frame, main.firstChild);
-})();
+    main.insertBefore(div, main.firstChild);
+}, 1000);
